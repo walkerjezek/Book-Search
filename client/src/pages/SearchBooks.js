c436ecb5808e609de24eeb03f76b5a5e7e987c1d
walkerj <wpjezek@gmail.com>
2022-07-21 18:47:41 -0600

    more debug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -64,6 +64,7 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -76,16 +77,26 @@ const SearchBooks = () => {
       return false;
     }
 
-    // Something needs to happen here? -----------------------------------------------------------------------------
+    // Refactor.... -----------------------------------------------------------------------------
     try {
-      const response = await saveBook(bookToSave, token);
+      await saveBook({
+        variables: {book: bookToSave},
+        update(cache, { data: { saveBook }}) {
+          try {
+            cache.writeQuery({
+              query: GET_ME,
+              data: { me: saveBook}
+            });
+          } catch (err) {
+            console.log(err);
+          }
+        }
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      });
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      
     } catch (err) {
       console.error(err);
     }
